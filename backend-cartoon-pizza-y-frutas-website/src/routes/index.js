ff9baa0f7592814e7be2105054200d52c0a215dc
Dilan <dilanespindola30@gmail.com>
2021-10-22 18:09:33 -0500

    envio de formulario contacto

--- backend-cartoon-pizza-y-frutas-website/src/routes/index.js
+++ backend-cartoon-pizza-y-frutas-website/src/routes/index.js
@@ -1,15 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer =  require('nodemailer');
+const { google } = require('googleapis');
+const { oauth2 } = require('googleapis/build/src/apis/oauth2');
+const { gmail } = require('googleapis/build/src/apis/gmail');
+
 
 router.get('/', (req, res) => {
     res.send('welcome');
 })
 
 router.post('/enviar-contacto', (req, res) => {
-    const {asunto, nombre, email, mensaje, datos} = req.body;
-    console.log(req.body)
-    res.redirect('http://localhost:3000/enviar-contacto')
-    //console.log(datos.email)
+    const {asunto, nombre, email, mensaje} = req.body;
+    
+    const contentHtml = `
+        <h1>Informacion de la persona:</h1>
+        <ul>
+            <li>Asunto: ${asunto} </li>
+            <li>Nombre: ${nombre} </li>
+            <li>Correo: ${email} </li>
+            <li>Mensaje: ${mensaje} </li>
+        </ul>
+    `;
+
+    const clientID = '59869306709-pfdpllnp06q6kbguilck18t2f5dvfde8.apps.googleusercontent.com';
+    const clientSecret = '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';
+    const refreshToken = '1//0482Yyo4ZHiQdCgYIARAAGAQSNwF-L9Irx9xFl-3GW4l57qZTvA1r1HDgaEX4pIPNky4XgsYGJrBbvtU1VyOk6mJRkjfqgoeBv8k';
+    const redirectURI = 'https://developers.google.com/oauthplayground';
+
+    const oAuth2Client = new google.auth.OAuth2(clientID, clientSecret, redirectURI);
+    oAuth2Client.setCredentials({refresh_token: refreshToken});
+
+    console.log('todo bien')
+
+    async function sendMail() {
+        try {
+            const accessToken = await oAuth2Client.getAccessToken();
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    type : "OAuth2",
+                    user : 'cartooncliente@gmail.com',
+                    clientId : clientID,
+                    clientSecret : clientSecret,
+                    refreshToken : refreshToken,
+                    accessToken : accessToken
+                }
+            })
+
+            let destinatario = 'cartooncliente@gmail.com';
+
+            const options = {
+                from : email,
+                to : destinatario,
+                subject : "Informacion de la persona",
+                html : contentHtml,
+            }
+            const res = await transporter.sendMail(options);
+            return res;
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    sendMail()
+        .then(result => {
+            res.redirect('http://localhost:3000/enviar-contacto')
+            console.log('sent', result)
+        })
+        .catch(error => console.log(error));
+        
 });
 
 module.exports = router;
