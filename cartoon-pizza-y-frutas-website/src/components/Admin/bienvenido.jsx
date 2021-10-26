db2a15eac5763a4c5a7907fcdc330ff9f8e5b817
Juan Pablo Insuasty Murillo <88408551+Soachi99@users.noreply.github.com>
2021-10-26 16:56:06 -0500

    Correciones y seguro de administrador

--- cartoon-pizza-y-frutas-website/src/components/Admin/bienvenido.jsx
+++ cartoon-pizza-y-frutas-website/src/components/Admin/bienvenido.jsx
@@ -4,16 +4,30 @@ import "./bienvenido.css"
 
 class Bienvenido extends React.Component {
     render() {
-        return (
-            <div className = "bienvenido-principal">
-                
-                <div className="bienvenido carousel-caption">
-                    <h1 className = "mt-5"> <b> Bienvenido admistrador </b> </h1>
-                    <img src={process.env.PUBLIC_URL + "/images/Logo.png"} className="logo-admin img-fluid d-block mx-auto mt-5"  alt="Logo admin" />
+        if(localStorage.getItem('admin_view') == null || localStorage.getItem('admin_view') === false)
+        {
+            return (
+                <div className = "bienvenido-principal">                
+                    <div className="bienvenido carousel-caption">
+                        <h1 className = "mt-5"> <b> No tienes permisos para entrar a esta pagina o no te has logeado </b> </h1>
+                        <img src={process.env.PUBLIC_URL + "/images/Logo.png"} className="logo-admin img-fluid d-block mx-auto mt-5"  alt="Logo admin" />
+                    </div>
+    
                 </div>
-
-            </div>
-        );
+            );
+        }
+        else{
+            return (
+                <div className = "bienvenido-principal">                
+                    <div className="bienvenido carousel-caption">
+                        <h1 className = "mt-5"> <b> Bienvenido admistrador </b> </h1>
+                        <img src={process.env.PUBLIC_URL + "/images/Logo.png"} className="logo-admin img-fluid d-block mx-auto mt-5"  alt="Logo admin" />
+                    </div>
+    
+                </div>
+            );
+        }
+        
     }
 }
 
