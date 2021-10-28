4ffae43168884cade5698c34a19f0db8536dd3ec
Dilan <dilanespindola30@gmail.com>
2021-10-28 18:01:14 -0500

    ajuste menor

--- backend-cartoon-pizza-y-frutas-website/src/routes/index.js
+++ backend-cartoon-pizza-y-frutas-website/src/routes/index.js
@@ -29,9 +29,7 @@ router.post('/enviar-contacto', (req, res) => {
     const redirectURI = 'https://developers.google.com/oauthplayground';
 
     const oAuth2Client = new google.auth.OAuth2(clientID, clientSecret, redirectURI);
-    oAuth2Client.setCredentials({refresh_token: refreshToken});
-
-    console.log('todo bien')
+    oAuth2Client.setCredentials({refresh_token: refreshToken});    
 
     async function sendMail() {
         try {
@@ -70,6 +68,81 @@ router.post('/enviar-contacto', (req, res) => {
         })
         .catch(error => console.log(error));
         
+});
+
+router.post('/envio-reserva', (req, res) => {
+    const { 
+        nombre, 
+        email, 
+        telefono, 
+        numPersonas, 
+        servicio, 
+        fecha, 
+        hora, 
+        mensaje 
+    } = req.body;
+    console.log(req.body)
+
+    const contentHtml = `
+        <h1>Informacion de la reserva:</h1>
+        <ul>
+            <li>nombre: ${nombre} </li>
+            <li>Correo: ${email} </li>
+            <li>Telefono: ${telefono} </li>
+            <li>Numero de personas: ${numPersonas} </li>
+            <li>Servicio: ${servicio} </li>
+            <li>Fecha: ${fecha} </li>
+            <li>Hora: ${hora} </li>                        
+        </ul>
+        <p>Indicaciones especiales: ${mensaje} </p>
+    `;
+
+    const clientID = '59869306709-pfdpllnp06q6kbguilck18t2f5dvfde8.apps.googleusercontent.com';
+    const clientSecret = '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';
+    const refreshToken = '1//0482Yyo4ZHiQdCgYIARAAGAQSNwF-L9Irx9xFl-3GW4l57qZTvA1r1HDgaEX4pIPNky4XgsYGJrBbvtU1VyOk6mJRkjfqgoeBv8k';
+    const redirectURI = 'https://developers.google.com/oauthplayground';
+
+    const oAuth2Client = new google.auth.OAuth2(clientID, clientSecret, redirectURI);
+    oAuth2Client.setCredentials({refresh_token: refreshToken});
+
+    async function sendMail() {
+        try {
+            const accessToken = await oAuth2Client.getAccessToken();
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    type : "OAuth2",
+                    user : 'cartooncliente@gmail.com',
+                    clientId : clientID,
+                    clientSecret : clientSecret,
+                    refreshToken : refreshToken,
+                    accessToken : accessToken
+                }
+            })
+
+            let destinatarios = ['cartooncliente@gmail.com', email];
+
+            const options = {
+                from : 'cartooncliente@gmail.com',
+                to : destinatarios,
+                subject : "Informacion de la reserva",
+                html : contentHtml,
+            }
+            const res = await transporter.sendMail(options);
+            return res;
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    sendMail()
+        .then(result => {
+            res.redirect('http://localhost:3000/admin/reservas')
+            console.log('sent', result)
+        })
+        .catch(error => console.log(error));
+
+    
 });
 
 module.exports = router;
