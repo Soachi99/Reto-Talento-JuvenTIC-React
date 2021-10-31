91c906e527f1f896f3e668c72b57d28dc6df7345
Dilan <dilanespindola30@gmail.com>
2021-10-31 00:52:55 -0500

    make it better

--- backend-cartoon-pizza-y-frutas-website/src/index.js
+++ backend-cartoon-pizza-y-frutas-website/src/index.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 // settings
-app.set('port', process.env.PORT || 4000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -21,6 +21,6 @@ app.use(require('./routes'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // server 
-app.listen(app.get('port'), () => {
-    console.log('Servidor ejecuntadonse en puerto', app.get('port'));
+app.listen(port, () => {
+    console.log('Servidor ejecuntadonse en puerto', port);
 });
