8cbb5007891f5528a4c71760deb08ab3c2ce32bb
Dilan <dilanespindola30@gmail.com>
2021-11-03 14:03:17 -0500

    se removio el backend

--- backend-cartoon-pizza-y-frutas-website/src/public/js/mensajeReserva.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    swal({
-        title : 'Envio Exitoso',
-        text : 'La reserva se ha enviado a tu correo electronico 😀',
-        icon : 'success',
-        button : 'OK' 
-    });
-})
