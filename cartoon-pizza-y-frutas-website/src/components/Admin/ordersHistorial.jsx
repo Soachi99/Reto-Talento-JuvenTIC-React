85843152dfabbb23fc164f08d6f299d99ac91231
Juan Pablo Insuasty Murillo <88408551+Soachi99@users.noreply.github.com>
2021-11-04 15:05:21 -0500

    Historial de pedidos sin productos actualizado

--- cartoon-pizza-y-frutas-website/src/components/Admin/ordersHistorial.jsx
+++ cartoon-pizza-y-frutas-website/src/components/Admin/ordersHistorial.jsx
@@ -16,17 +16,29 @@ class HistorialPedidos extends React.Component {
             );
         }
         else {
-            return (
-                <>
+
+            if (localStorage.getItem('pedidos') === null) {
+                return (
                     <div className="titulo">
                         <h1 className="fs-1 fw-bold ms-5 mt-4">Historial de pedidos</h1>
-                        <h2 className="ms-5 mt-4 fs-3">Consulta los pedidos realizados en la pagina</h2>
+                        <h2 className="texto-sin-productos ms-5 mt-4 fs-3">No hay pedidos que mostrar por el momento</h2>
                     </div>
-                    <div className="historial-container mb-5">
-                        <Ordenes />
-                    </div>
-                </>
-            );
+                );
+            }
+            else {
+                return (
+                    <>
+                        <div className="titulo">
+                            <h1 className="fs-1 fw-bold ms-5 mt-4">Historial de pedidos</h1>
+                            <h2 className="ms-5 mt-4 fs-3">Consulta los pedidos realizados en la pagina</h2>
+                        </div>
+                        <div className="historial-container mb-5">
+                            <Ordenes />
+                        </div>
+                    </>
+                );
+            }
+
         }
     }
 }
