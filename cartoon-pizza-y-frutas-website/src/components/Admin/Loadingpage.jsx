163909e31cc10dc009124faaef0e7a4757109241
Juan Pablo Insuasty Murillo <88408551+Soachi99@users.noreply.github.com>
2021-11-26 12:48:47 -0500

    Mejora en el estilo responsive de la pantalla de carga

--- cartoon-pizza-y-frutas-website/src/components/Admin/Loadingpage.jsx
+++ cartoon-pizza-y-frutas-website/src/components/Admin/Loadingpage.jsx
@@ -3,9 +3,9 @@ import React from "react";
 class LoadingPage extends React.Component {
     render() { 
         return  (<div>
-            <div className = "bienvenido-principal">                
+            <div className = "Cargando">                
                 <div className="bienvenido carousel-caption">
-                    <h1 className = "mt-5"> <b> Espera un momento administrador mientras te redirigimos correctamente </b> </h1>
+                    <h1 className = "mt-5 me-3 ms-3"> <b> Espera un momento administrador mientras te redirigimos correctamente </b> </h1>
                     <img src={process.env.PUBLIC_URL + "/images/Logo.png"} className="logo-admin img-fluid d-block mx-auto mt-5"  alt="Logo admin" />
                 </div>
         
